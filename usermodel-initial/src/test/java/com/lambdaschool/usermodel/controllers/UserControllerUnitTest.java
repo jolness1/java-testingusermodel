f5b6b9c3573f28ee7a35ead4501a92ad773abfe4
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2021-02-24 18:44:59 -0700

    progress

---
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/controllers/UserControllerUnitTest.java
@@ -0,0 +1,197 @@
+package com.lambdaschool.usermodel.controllers;
+
+import com.lambdaschool.usermodel.UserModelApplicationTesting;
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
+import com.lambdaschool.usermodel.models.Useremail;
+import com.lambdaschool.usermodel.services.RoleService;
+import com.lambdaschool.usermodel.services.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@WithMockUser(username = "admin",
+        roles = {"USER", "ADMIN"})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    classes = UserModelApplicationTesting.class,
+    properties = {
+        "command.line.runner.enable=false"})
+@AutoConfigureMockMvc
+public class UserControllerUnitTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private RoleService roleService;
+
+    private List<User> userList;
+
+
+    @Before
+    public void setUp() throws Exception {
+        userService.deleteAll();
+        roleService.deleteAll();
+        Role r1 = new Role("admin");
+        Role r2 = new Role("user");
+        Role r3 = new Role("data");
+
+        r1 = roleService.save(r1);
+        r2 = roleService.save(r2);
+        r3 = roleService.save(r3);
+
+        User u1 = new User("admin",
+                "password",
+                "admin@lambdaschool.local");
+        u1.getRoles()
+                .add(new UserRoles(u1,
+                        r1));
+        u1.getRoles()
+                .add(new UserRoles(u1,
+                        r2));
+        u1.getRoles()
+                .add(new UserRoles(u1,
+                        r3));
+        u1.getUseremails()
+                .add(new Useremail(u1,
+                        "admin@email.local"));
+        u1.getUseremails()
+                .add(new Useremail(u1,
+                        "admin@mymail.local"));
+
+        userService.save(u1);
+
+        // data, user
+        User u2 = new User("cinnamon",
+                "1234567",
+                "cinnamon@lambdaschool.local");
+        u2.getRoles()
+                .add(new UserRoles(u2,
+                        r2));
+        u2.getRoles()
+                .add(new UserRoles(u2,
+                        r3));
+        u2.getUseremails()
+                .add(new Useremail(u2,
+                        "cinnamon@mymail.local"));
+        u2.getUseremails()
+                .add(new Useremail(u2,
+                        "hops@mymail.local"));
+        u2.getUseremails()
+                .add(new Useremail(u2,
+                        "bunny@email.local"));
+        userService.save(u2);
+
+        // user
+        User u3 = new User("barnbarn",
+                "ILuvM4th!",
+                "barnbarn@lambdaschool.local");
+        u3.getRoles()
+                .add(new UserRoles(u3,
+                        r2));
+        u3.getUseremails()
+                .add(new Useremail(u3,
+                        "barnbarn@email.local"));
+        userService.save(u3);
+
+        User u4 = new User("puttat",
+                "password",
+                "puttat@school.lambda");
+        u4.getRoles()
+                .add(new UserRoles(u4,
+                        r2));
+        userService.save(u4);
+
+        User u5 = new User("misskitty",
+                "password",
+                "misskitty@school.lambda");
+        u5.getRoles()
+                .add(new UserRoles(u5,
+                        r2));
+        userService.save(u5);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void listAllUsers() throws Exception
+    {
+        String apiUrl = "/users/users";
+        Mockito.when(userService.findAll())
+            .thenReturn(userList);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl)
+            .accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb)
+            .andReturn();
+        String tr = r.getResponse()
+            .getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(userList);
+
+        System.out.print(er);
+        assertEquals(er, tr);
+    }
+
+    @Test
+    public void getUserById() {
+    }
+
+    @Test
+    public void getUserByName() {
+    }
+
+    @Test
+    public void getUserLikeName() {
+    }
+
+    @Test
+    public void addNewUser() {
+    }
+
+    @Test
+    public void updateFullUser() {
+    }
+
+    @Test
+    public void updateUser() {
+    }
+
+    @Test
+    public void deleteUserById() {
+    }
+
+    @Test
+    public void getCurrentUserInfo() {
+    }
+}
